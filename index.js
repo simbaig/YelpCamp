5119608ce39b4221bcebcdaca8748921b35b08f8
wasim-ay <wasimbaig000@gmail.com>
2022-03-14 17:29:55 +0530

    add basic auth

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
+const User = require("./models/user");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+
+mongoose
+	.connect("mongodb://localhost:27017/yelp-camp")
+	.then(() => {
+		console.log("MONGO CONNECTION OPEN!!!");
+	})
+	.catch((err) => {
+		console.log("OH NO MONGO CONNECTION ERROR");
+		console.log(err);
+	});
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+	secret: "wasimbaig",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+	next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = "Oh No, Something Went Wrong!";
+	res.status(statusCode).render("error", { err });
+});
+
+app.listen(3000, () => {
+	console.log("LISTENING ON PORT 3000");
+});
