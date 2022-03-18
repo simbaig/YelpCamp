904ca45f98220ac15519503a5584182b60c16d56
wasim-ay <wasimbaig000@gmail.com>
2022-03-18 20:19:53 +0530

    finish up image upload and thumbnail virtual

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+	url: String,
+	filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+	return this.url.replace("/upload", "/upload/ar_4:3,c_crop");
+});
+
 const CampgroundSchema = new Schema({
 	title: String,
-	image: String,
+	images: [ImageSchema],
 	price: Number,
 	description: String,
 	location: String,
